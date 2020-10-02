0be9e4dcb4e9ed6865349c17021dfe10c3306ee4
Murat Gözel <murat@gozel.com.tr>
2020-10-02 18:15:02 +0300

    Checking img/ folder serving problem.

--- monodrom.config.js
+++ monodrom.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "runtime": {
     "pkgname": "watermark-utility.gozel.com.tr",
-    "version": "20.10.1",
+    "version": "20.10.2",
     "env": "production",
     "name": "Watermark Utility",
     "description": "Apply watermark to your social media photos.",
